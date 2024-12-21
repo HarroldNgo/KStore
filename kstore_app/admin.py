45ce8de1b701edb0a74db4d6d033325829fe1c45
Lucky2190 <harroldngo@gmail.com>
2024-12-21 10:20:45 -0500

    small correction

--- kstore_app/admin.py
+++ kstore_app/admin.py
@@ -7,7 +7,7 @@ class CartAdmin(admin.ModelAdmin):
     search_fields = ['cart_code', 'user__username', 'user__email', 'user__first_name', 'user__last_name']
     list_filter = ['paid']
 class CartItemAdmin(admin.ModelAdmin):
-    search_fields = ['cart__user__username', 'cart__user__email', 'cart__user__first_name', 'cart__user__last_name',
+    search_fields = ['cart__cart_code','cart__user__username', 'cart__user__email', 'cart__user__first_name', 'cart__user__last_name',
                      'item__name', 'item__description', 'item__flavour', 'item__category' ]
 
 admin.site.register(Item, ItemAdmin)
